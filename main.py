ca780ccf29ae2c33e6d527b9f6556d89be5dae0d
StremCipher <47041923+StremCipher@users.noreply.github.com>
2021-10-18 01:50:08 +0530

    initial commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pickle
 import pandas as pd
 import json
 from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
+app = Flask('cp-tool')
 
 
 @app.route("/", methods=['POST', 'GET'])
